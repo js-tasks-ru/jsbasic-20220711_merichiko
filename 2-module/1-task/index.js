257b1f9298dbbcc8a662bda167f59ccecacf14e0
Ekaterina Bobrova <merichiko@yandex.ru>
2022-07-21 14:09:19 +0300

    Занятие 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (key in salaries) {
+    if (typeof salaries[key] == 'number') {
+      sum += salaries[key];
+    }
+  }
+
+  return sum;
 }
+
+console.log(sum(salaries));
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
-};
+    sum() {
+      return this.a + this.b;
+    },
+  
+    mul() {
+      return this.a * this.b;
+    },
+  
+    read() {
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    }
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
