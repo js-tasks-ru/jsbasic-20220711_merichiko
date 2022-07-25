ef4ee0201ad83d9a7ef44522156e4552d614cfaf
Ekaterina Bobrova <merichiko@yandex.ru>
2022-07-25 14:39:39 +0300

    Module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,11 +1,10 @@
 function sumSalary(salaries) {
   let sum = 0;
-  for (key in salaries) {
-    if (typeof salaries[key] == 'number') {
+  for (let key in salaries) {
+    if ((typeof salaries[key] == "number") && (isFinite(salaries[key]))) {
       sum += salaries[key];
     }
   }
-
   return sum;
 }
 
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,21 +1,15 @@
 let calculator = {
-    sum() {
-      return this.a + this.b;
-    },
-  
-    mul() {
-      return this.a * this.b;
-    },
-  
-    read() {
-      this.a = +prompt('a?', 0);
-      this.b = +prompt('b?', 0);
-    }
-  };
-  
-  calculator.read();
-  alert( calculator.sum() );
-  alert( calculator.mul() );
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b;
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  },
+};
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
